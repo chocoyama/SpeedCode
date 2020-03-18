6f82928ec5e7e54e30dbca0bc98786df563e949d
Takuya Yokoyama <chocoyama@yahoo-corp.jp>
2020-03-18 10:24:07 +0900

    Initial Commit

---
+++ SpeedCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpeedCode
+//
+//  Created by Takuya Yokoyama on 2020/03/18.
+//  Copyright © 2020 Takuya Yokoyama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
